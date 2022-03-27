1f322b39736511e8a933476b850d80960c45a5b4
Nikita Parshin <68809831+lParzivall@users.noreply.github.com>
2022-03-27 21:08:50 +0300

    Update UserserviceApplication.java

--- src/main/java/ru/mitrasoft/userservice/UserserviceApplication.java
+++ src/main/java/ru/mitrasoft/userservice/UserserviceApplication.java
@@ -23,27 +23,4 @@ public class UserserviceApplication {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-//	@Bean
-//	CommandLineRunner run(AppUserService appUserService) {
-//		return args -> {
-//			appUserService.saveAppUserRole(new AppUserRole(null, "ROLE_USER"));
-//			appUserService.saveAppUserRole(new AppUserRole(null, "ROLE_MANAGER"));
-//			appUserService.saveAppUserRole(new AppUserRole(null, "ROLE_ADMIN"));
-//			appUserService.saveAppUserRole(new AppUserRole(null, "ROLE_SUPER_ADMIN"));
-//
-//			appUserService.saveAppUser(new AppUser(null, "John Travolta", "john@gfhgh.ry", "1234", new ArrayList<>()));
-//			appUserService.saveAppUser(new AppUser(null, "Will Smith", "will@sdf.ty", "1234", new ArrayList<>()));
-//			appUserService.saveAppUser(new AppUser(null, "Jim Carry", "jim@rdgdfg.tu", "1234", new ArrayList<>()));
-//			appUserService.saveAppUser(new AppUser(null, "Arnold Schwarzenegger", "arnold@afsdf.ry", "1234", new ArrayList<>()));
-//
-////			appUserService.addRoleToAppUser("john@gfhgh.ry", "ROLE_USER");
-////			appUserService.addRoleToAppUser("will@sdf.ty", "ROLE_MANAGER");
-////			appUserService.addRoleToAppUser("jim@rdgdfg.tu", "ROLE_ADMIN");
-////			appUserService.addRoleToAppUser("arnold@afsdf.ry", "ROLE_SUPER_ADMIN");
-//			appUserService.addRoleToAppUser("arnold@afsdf.ry", "ROLE_ADMIN");
-////			appUserService.addRoleToAppUser("arnold@afsdf.ry", "ROLE_USER");
-//		};
-//	}
-
 }
