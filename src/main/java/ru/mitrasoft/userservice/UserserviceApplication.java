8ebda74967fd80279e1a7b71923b1fb7033c7fce
Nikita Parshin <68809831+lParzivall@users.noreply.github.com>
2022-03-27 21:30:08 +0300

    Update UserserviceApplication.java

--- src/main/java/ru/mitrasoft/userservice/UserserviceApplication.java
+++ src/main/java/ru/mitrasoft/userservice/UserserviceApplication.java
@@ -1,26 +1,20 @@
 package ru.mitrasoft.userservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.mitrasoft.userservice.domain.AppUser;
-import ru.mitrasoft.userservice.domain.AppUserRole;
-import ru.mitrasoft.userservice.service.AppUserService;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
